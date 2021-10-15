228df75781a2868fc5a0d1b95c1e423403e5dd01
Jackicroquant <tangotel.tellier@gmail.com>
2021-10-15 13:36:29 +0200

    Peugeot

--- Voiture.scala
+++ Voiture.scala
@@ -1,4 +1,22 @@
-sealed abstract class Vehicule[A]{}
+
+// Exo sur la class Vehicule
+
+sealed abstract class Vehicule(){}
 case class Car() extends Vehicule(){}
 case class Motorbike() extends Vehicule(){}
 case class Boat() extends Vehicule(){}
+
+// Exo sur la filiale Peugeot
+
+sealed abstract class Peugeot(){
+  val model : String
+}
+
+abstract class Vehicule() extends Peugeot(){}
+case class Car(model:String) extends Vehicule(){}
+case class Motorbike(model:String) extends Vehicule(){}
+case class Boat(model:String) extends Vehicule(){}
+case class Bike(model:String) extends Vehicule(){}
+
+abstract class ElectricalGood() extends Peugeot(){}
+case class CoffeeMachine(model:String) extends ElectricalGood(){}
