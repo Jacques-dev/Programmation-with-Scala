a6061827a21caa09eed43d005110f6a8ad6d57f5
Jackicroquant <tangotel.tellier@gmail.com>
2021-10-29 15:56:32 +0200

    x

--- projet/src/main/scala/projet.scala
+++ projet/src/main/scala/projet.scala
@@ -2,15 +2,19 @@ import scala.collection.MapView
 import scala.io.BufferedSource
 import scala.io.Source
 
+
+
 object PROJET {
-  
-  def loadCSV(filename: String): {
-    val file: BufferedSource = Source.fromFile("resources/".filename.".csv")
-    val partitions: Map[String, Iterable[Double]] = getRatingsByDepartement(file.getLines().to(Iterable))
-  }
-  
-  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+  def loadData(filename: String): DataFrame = {
+    val df = spark.read.csv("resources/"+filename+".csv")
+    df.printSchema()
   }
 
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+    
+    val airports = loadData("airports")
+    val countries = loadData("countries")
+    val runways = loadData("runways")
+  }
 }
